2f9013f4b63c415aa7909cd54789e3ade141fa62
nicolaslimadev <nicolaslima.dev.web@gmail.com>
2025-10-24 13:40:29 -0300

    feat: Deletando o component de Formulário, centralizando ele direto no "pai" App.tsx. O formulário já está adicionando e exibindo o card na tela;

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
+import { useState } from 'react'
 import './App.css';
 import CardPost from './components/CardPost/CardPost';
-import Formulario from './components/Form/Formulario';
+
+export interface PostProps {
+  nome: string,
+  mensagem: string
+}
 
 export default function App(){
+  const [inputNome, setInputNome] = useState('')
+  const [inputMensagem, setInputMensagem] = useState('')
+  const [posts, setPosts] = useState<PostProps[]>([])
+
+  function handleAdd(){
+    setPosts([...posts, {nome: inputNome, mensagem: inputMensagem}])
+
+    setInputNome('')
+    setInputMensagem('')
+  }
+
   return(
     <main className='main-container'>
       <h1 className='title-main'>Post-it</h1>
 
-      <Formulario />
+        <div className='container-form'>
+         <input 
+            className='input-nome'
+            type="text"
+            placeholder='Nome do post-it'
+            value={inputNome}
+            onChange={(e)=> setInputNome(e.target.value)}
+            required
+         />
+
+         <input
+            className='input-mensagem' 
+            type="text"
+            placeholder='Mensagem'
+            value={inputMensagem}
+            onChange={(e)=> setInputMensagem(e.target.value)}
+            required
+         />
+
+         <button onClick={handleAdd} className='btn-criar'>Criar</button>
+      </div>
 
       <section className='post-container'>
-        <CardPost />
+        {posts.map((post, index) => (
+          <CardPost key={index} nome={post.nome} mensagem={post.mensagem} />
+        ))}
       </section>
     </main>
   )
